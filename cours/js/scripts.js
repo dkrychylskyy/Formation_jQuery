98a2e5411b0cff841f4b06ae4e01e4acadafa104
dkrychylskyy <d.krychylskyy@gmail.com>
2017-11-05 20:39:15 +0100

    scripts.js et index.html sont propers par defaut

--- cours/js/scripts.js
+++ cours/js/scripts.js
@@ -1,13 +1,3 @@
 $(function () {
-    function traitement() {
-        alert('Image cliquée');
-    }
-    $('#activer').on('click', function () {
-        $('#image').click(traitement);
-    });
-    $('#desactiver').on('click', function () {
-        $('#image').off('click', traitement);
-        
-        
-    });
+    
 })
