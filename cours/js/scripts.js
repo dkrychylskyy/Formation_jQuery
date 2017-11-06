f9d99eae22c711881d44097e68884a3e3c5889ec
dkrychylskyy <d.krychylskyy@gmail.com>
2017-11-06 07:53:55 +0100

    Animation avec un modèle de progression

--- cours/js/scripts.js
+++ cours/js/scripts.js
@@ -3,9 +3,9 @@ $(function () {
     $('td').css('width','200px');
     $('td').css('text-align','center');
     $('#affiche').click(function() {
-      $('tr:even').show('slow');
+      $('tr:even').show('slow', 'linear');
     } ); 
     $('#cache').click(function() {
-      $('tr:even').hide(1000);
+      $('tr:even').hide(1000, 'swing');
     });
 })
