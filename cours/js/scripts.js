9daa6696921b151db6f463ac9053b51effeb02b4
dkrychylskyy <d.krychylskyy@gmail.com>
2017-11-06 07:51:26 +0100

    apparition et disparition avec une animation

--- cours/js/scripts.js
+++ cours/js/scripts.js
@@ -1,3 +1,11 @@
 $(function () {
-    
+    $('tr:even').css('background','yellow');
+    $('td').css('width','200px');
+    $('td').css('text-align','center');
+    $('#affiche').click(function() {
+      $('tr:even').show('slow');
+    } ); 
+    $('#cache').click(function() {
+      $('tr:even').hide(1000);
+    });
 })
